15c80b50dc401739a2d30d7f7a96fbdbaa289f68
Đặng Văn Luyện <133982516+LuyenVPN@users.noreply.github.com>
2024-11-21 16:34:08 +0700

    bai3laydiem.c

--- bai3laydiem.c
+++ bai3laydiem.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
 void tinhTichMaTran(int n, int m, int A[][100]) {
-    int At[100][100];    // Ma trận chuyển vị của A
-    int C[100][100];     // Ma trận tích
-
-    // Tạo ma trận chuyển vị At
+    int At[100][100];   
+    int C[100][100];    
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             At[j][i] = A[i][j];
         }
     }
-
-    // Tính ma trận tích C = A * At
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             C[i][j] = 0;
@@ -21,7 +17,6 @@ void tinhTichMaTran(int n, int m, int A[][100]) {
         }
     }
 
-    // In kết quả
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf("%d ", C[i][j]);
@@ -32,28 +27,24 @@ void tinhTichMaTran(int n, int m, int A[][100]) {
 
 int main() {
     int T;
-    printf("Nhập số bộ test: ");
+    printf("Nhap so bo test: ");
     scanf("%d", &T);
 
     for (int t = 1; t <= T; t++) {
         int n, m;
-        printf("Nhập số hàng và số cột của ma trận (n m): ");
+        printf("Nhap so hang va so cot cua ma tran (n m): ");
         scanf("%d %d", &n, &m);
 
         int A[100][100];
 
-        // Nhập ma trận A
-        printf("Nhập ma trận A:\n");
+     
+        printf("Nhap ma tran A:\n");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 scanf("%d", &A[i][j]);
             }
         }
-
-        // In thứ tự bộ test
         printf("Test %d:\n", t);
-
-        // Gọi hàm tính tích ma trận
         tinhTichMaTran(n, m, A);
     }
 
