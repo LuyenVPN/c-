f21c272e86819dec5e51ce4064dd4a4e5cfc0d0e
Đặng Văn Luyện <133982516+LuyenVPN@users.noreply.github.com>
2024-11-21 16:35:33 +0700

    bai1thuchanh.c

--- bai1thuchanh.c
+++ bai1thuchanh.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#define MAX_VAL 30001 // Giá tr? t?i da trong dãy
-#define MAX_N 100     // S? ph?n t? t?i da
+#define MAX_VAL 30001 
+#define MAX_N 100     
 
 int main() {
     int n, a[MAX_N];
-    int freq[MAX_VAL] = {0}; // M?ng luu t?n su?t các giá tr?
+    int freq[MAX_VAL] = {0};
     int i, maxFreq = 0, maxValue = -1;
 
     printf("Nhap so phan tu cua day (khong qua 100): ");
@@ -14,11 +14,9 @@ int main() {
     for (i = 0; i < n; i++) {
         scanf("%d", &a[i]);
         if (a[i] >= 0 && a[i] < MAX_VAL) {
-            freq[a[i]]++; // Tang t?n su?t c?a giá tr? a[i]
+            freq[a[i]]++; 
         }
     }
-
-    // Tìm giá tr? xu?t hi?n nhi?u nh?t
     for (i = 0; i < MAX_VAL; i++) {
         if (freq[i] > maxFreq) {
             maxFreq = freq[i];
