be591d287cdb3d647d9ec96c05aff726a24e917a
Đặng Văn Luyện <133982516+LuyenVPN@users.noreply.github.com>
2024-11-21 17:08:39 +0700

    b1thuchanh.cpp

--- b1thuchanh.cpp
+++ b1thuchanh.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#define MAX_VAL 30001 // Gi· tr? t?i da trong d„y
+#define MAX_VAL 30001 // Gi√° tr? t?i da trong d√£y
 #define MAX_N 100     // S? ph?n t? t?i da
 
 int main() {
     int n, a[MAX_N];
-    int freq[MAX_VAL] = {0}; // M?ng luu t?n su?t c·c gi· tr?
+    int freq[MAX_VAL] = {0}; // M?ng luu t?n su?t c√°c gi√° tr?
     int i, maxFreq = 0, maxValue = -1;
 
     printf("Nhap so phan tu cua day (khong qua 100): ");
@@ -14,11 +14,11 @@ int main() {
     for (i = 0; i < n; i++) {
         scanf("%d", &a[i]);
         if (a[i] >= 0 && a[i] < MAX_VAL) {
-            freq[a[i]]++; // Tang t?n su?t c?a gi· tr? a[i]
+            freq[a[i]]++; // Tang t?n su?t c?a gi√° tr? a[i]
         }
     }
 
-    // TÏm gi· tr? xu?t hi?n nhi?u nh?t
+    // T√¨m gi√° tr? xu?t hi?n nhi?u nh?t
     for (i = 0; i < MAX_VAL; i++) {
         if (freq[i] > maxFreq) {
             maxFreq = freq[i];
